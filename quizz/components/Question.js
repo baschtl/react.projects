cbf8138c4e46aa1e7851b2c62c0a8225a25fe59c
Sebastian Oelke <dev@soelke.de>
2022-09-15 14:59:19 +0200

    Only allow selection of one answer per question.

--- quizz/components/Question.js
+++ quizz/components/Question.js
@@ -1,11 +1,31 @@
 import React from "react"
 
 export default function Question(props) {
+  function answerSelectable(answerId) {
+    const currentSelected = props.answers.find(e => e.selected)
+
+    // No answer selected
+    if (currentSelected === undefined) {
+      return true
+    }
+
+    // Allow un-selecting an already selected answer
+    if (currentSelected && currentSelected.id === answerId) {
+      return true
+    }
+
+    // Another answer has been selected already
+    return false
+  }
+
   let answerElements = props.answers.map(e => {
     return <div
       key={e.id}
       className={`answer${ e.selected ? " selected" : ""}`}
-      onClick={() => props.onAnswerClick(props.id, e.id)}
+      onClick={() => {
+        answerSelectable(e.id) && props.onAnswerClick(props.id, e.id)
+      }
+    }
     >
       {atob(e.value)}
     </div>
