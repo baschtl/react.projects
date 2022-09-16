d38b37ac1fe6908811a3fed5741698e5dddce77d
Sebastian Oelke <dev@soelke.de>
2022-09-16 11:50:07 +0200

    Adds description.

--- quizz/components/StartQuiz.js
+++ quizz/components/StartQuiz.js
@@ -4,7 +4,8 @@ export default function StartQuiz(props) {
   return (
     <div className="screen-start">
       <h1>Quizzical</h1>
-      <p>A quiz game that challenges your knowledge!</p>
+      <p>A quiz game that challenges your knowledge</p>
+      <p><i>Select the answers that you think are correct. Find out how you did afterwards!</i></p>
       <button onClick={props.handleStartQuiz}>Start quiz</button>
     </div>
   )
