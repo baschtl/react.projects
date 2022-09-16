16366ccb0cfed4d147eadbf17a43de862cd13327
Sebastian Oelke <dev@soelke.de>
2022-09-16 11:48:26 +0200

    Uses real API call instead of dummy data.

--- quizz/components/App.js
+++ quizz/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {nanoid} from "nanoid"
+import { nanoid } from "nanoid"
 
 import StartQuiz from "./StartQuiz"
 import RunningQuiz from "./RunningQuiz"
@@ -7,19 +7,18 @@ import RunningQuiz from "./RunningQuiz"
 import { GameState } from "./enums"
 import { GameStateContext } from "./GameStateContext"
 
-let rawData = {"response_code":0,"results":[{"category":"R2VvZ3JhcGh5","type":"bXVsdGlwbGU=","difficulty":"ZWFzeQ==","question":"VGhlIGJvZHkgb2YgdGhlIEVneXB0aWFuIFNwaGlueCB3YXMgYmFzZWQgb24gd2hpY2ggYW5pbWFsPw==","correct_answer":"TGlvbg==","incorrect_answers":["QnVsbA==","SG9yc2U=","RG9n"]},{"category":"R2VuZXJhbCBLbm93bGVkZ2U=","type":"Ym9vbGVhbg==","difficulty":"bWVkaXVt","question":"VGhlIGJpa2luaSBpcyBuYW1lZCBhZnRlciB0aGUgIkJpa2luaSBBdG9sbCIsIGFuIGlzbGFuZCB3aGVyZSB0aGUgVW5pdGVkIFN0YXRlcyBjb25kdWN0ZWQgdGVzdHMgb24gYXRvbWljIGJvbWJzLg==","correct_answer":"VHJ1ZQ==","incorrect_answers":["RmFsc2U="]},{"category":"RW50ZXJ0YWlubWVudDogVmlkZW8gR2FtZXM=","type":"bXVsdGlwbGU=","difficulty":"bWVkaXVt","question":"SW4gd2hpY2ggTWFyaW8gZ2FtZSBkaWQgdGhlIE1lZ2EgTXVzaHJvb20gbWFrZSBpdHMgZGVidXQ\/","correct_answer":"TWFyaW8gUGFydHkgNA==","incorrect_answers":["TmV3IFN1cGVyIE1hcmlvIEJyb3Mu","TWFyaW8gS2FydCBXaWk=","U3VwZXIgTWFyaW8gM0QgV29ybGQ="]},{"category":"RW50ZXJ0YWlubWVudDogVmlkZW8gR2FtZXM=","type":"bXVsdGlwbGU=","difficulty":"ZWFzeQ==","question":"SW4gRG90YSAyLCBXcmFpdGggS2luZyB3YXMgcHJldmlvdXNseSBrbm93biBhcy4uLg==","correct_answer":"U2tlbGV0b24gS2luZw==","incorrect_answers":["UmVhcGVyIEtpbmc=","U2t1bGwgS2luZw==","SGVsbCBLaW5n"]},{"category":"RW50ZXJ0YWlubWVudDogSmFwYW5lc2UgQW5pbWUgJiBNYW5nYQ==","type":"Ym9vbGVhbg==","difficulty":"bWVkaXVt","question":"SW4gRnVsbCBNZXRhbCBQYW5pYyEsIFdoaXNwZXJlZCBhcmUgdGhvc2Ugd2hvIGFyZSBjYXBhYmxlIG9mIGNyZWF0aW5nIEJsYWNrIFRlY2hub2xvZ3ku","correct_answer":"VHJ1ZQ==","incorrect_answers":["RmFsc2U="]}]}
-
 export default function App() {
-  const [gameState, setGameState] = React.useState(GameState.RUNNING)
+  const [gameState, setGameState] = React.useState(GameState.START)
   const [questionData, setQuestionData] = React.useState([])
 
   React.useEffect(() => {
     if (gameState === GameState.RUNNING) {
-      // TODO: Use real API call after development
-      setQuestionData(generateQuestionData(rawData.results))
-      // fetch("https://opentdb.com/api.php?amount=5&encode=base64")
-      //   .then(res => res.json())
-      //   .then(data => setQuestionData(generateQuestionData(data.results)))
+      fetch("https://opentdb.com/api.php?amount=5&encode=base64")
+        .then(res => res.json())
+        .then(data => setQuestionData(generateQuestionData(data.results)))
+        .catch(error => {
+          console.error("Error fetching opentdb: ", error)
+        });
     }
   }, [gameState])
 
